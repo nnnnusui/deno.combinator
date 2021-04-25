06a261a65b44b95eeeed0c52ed003a8a63e4d3e5
nnnnusui <nnnnusui@gmail.com>
2021-04-25 14:39:53 +0900

    fix: mod.ts not being usable

--- mod.ts
+++ mod.ts
@@ -1,10 +1,9 @@
-export * from "./src/main/convert.ts";
+import chain from "./src/main/chain.ts";
+import convert from "./src/main/convert.ts";
+import not from "./src/main/not.ts";
+import option from "./src/main/option.ts";
+import or from "./src/main/or.ts";
+import repeat from "./src/main/repeat.ts";
+import use from "./src/main/use.ts";
 
-export * from "./src/main/chain.ts";
-export * from "./src/main/or.ts";
-
-export * from "./src/main/not.ts";
-export * from "./src/main/option.ts";
-export * from "./src/main/repeat.ts";
-
-export * from "./src/main/use.ts";
+export { chain, convert, not, option, or, repeat, use };

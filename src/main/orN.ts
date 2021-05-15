689cfe0bd27b2d1e266c5644fb924d86b2039bb6
nnnnusui <nnnnusui@gmail.com>
2021-05-16 03:36:59 +0900

    add: orN

---
+++ src/main/orN.ts
@@ -0,0 +1,20 @@
+import { Combinator } from "./type/Combinator.ts";
+import { or } from "./or.ts";
+import {
+  AnyCombinators,
+  Combinators,
+  ContextFrom,
+  Unified,
+} from "./type/Combinators.ts";
+
+export const orN = <T extends AnyCombinators>(
+  ...combinators: T
+): Combinator<ContextFrom<T>, Unified<T>> => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const it = (combinators: Combinators<ContextFrom<T>>): any => {
+    const [head, ...tails] = combinators;
+    if (tails.length < 1) return head;
+    return or(head, it(tails));
+  };
+  return it(combinators);
+};
